d15c4b23be52c0d3a4066947d7013d718b54cfcf
Niky Morgan <nicole.morgan@gmail.com>
2017-08-24 13:36:07 -0400

    Add prop

--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -7,7 +7,7 @@ const CategorySelector = (props) => {
   const categoryFields = categories.map(
     (category, i) => {
 
-      const checked = category === "... your code here"
+     const checked = category === props.activeCategory
 
       return (
         <CategoryField
